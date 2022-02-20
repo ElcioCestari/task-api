a2f0ad60510103f033b6a0bbfef3badb98a230d0
ElcioCestari <elciotaira@gmail.com>
2022-02-19 21:09:36 -0300

    add test find all

--- src/task/task.service.spec.ts
+++ src/task/task.service.spec.ts
@@ -1,10 +1,18 @@
+import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
 import { TaskService } from './task.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { notContains } from 'class-validator';
+
+const tasksFakes: Task[] = [];
+const task1 = new Task();
+const task2 = new Task();
+tasksFakes.push(task1);
+tasksFakes.push(task2);
 
 describe('TaskService', () => {
   let service: TaskService;
-  const repositoryMock = { getAllTasks: jest.fn() };
+  const repositoryMock = { find: jest.fn() };
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,5 +27,15 @@ describe('TaskService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('GetAllTasks', () => {
+    it('should be ruturn all tasks', async () => {
+      repositoryMock.find.mockResolvedValue(tasksFakes);
+      const result = await service.getAllTasks();
+
+      expect(result).toHaveLength(2);
+      expect(result).toEqual(tasksFakes);
+    });
   });
 });
