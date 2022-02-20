dc83dd82be85fc0c8f90e015b9a45d1992f9d6c9
ElcioCestari <elciotaira@gmail.com>
2022-02-20 13:46:22 -0300

    improve auth

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import {
+  isString,
+  IsString,
+  Matches,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(12)
   username: string;
+
+  @IsString()
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, {
+    message: 'password too week',
+  })
   password: string;
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,18 @@
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { EntityRepository, Repository } from 'typeorm';
+import { ConflictException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error);
+      throw new ConflictException('already exists user with this name');
+    }
   }
 }
