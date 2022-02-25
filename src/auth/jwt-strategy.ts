dba0c4f96e168de91dd198130f44f27d90d029b8
ElcioCestari <elciotaira@gmail.com>
2022-02-25 09:51:00 -0300

    feat:(main): JwtStrategy

---
+++ src/auth/jwt-strategy.ts
@@ -0,0 +1,30 @@
+import { JwtPayload } from './jwt-payload.interface';
+import { UsersRepository } from './users.repository';
+import { User } from './user.entity';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UsersRepository) private userRepository: UsersRepository,
+  ) {
+    super({
+      secrectOrKey: 'secrect_message',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User>{
+    const { username } = payload;
+    const user: User = await this.userRepository.findOne({ username });
+    if (!user) {
+      throw new UnauthorizedException(
+        'You dont have authorization to access this',
+      );
+    }
+    return user;
+  }
+}
